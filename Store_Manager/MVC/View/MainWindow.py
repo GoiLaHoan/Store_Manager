d5a897872426b84a66b0f61d4a76a5a8f0958847
Nguyễn Văn Hoàn <nvhoan911@gmail.com>
2020-11-29 16:32:24 +0700

    Hoan

--- Store_Manager/MVC/View/MainWindow.py
+++ Store_Manager/MVC/View/MainWindow.py
@@ -48,68 +48,72 @@ def mainwindow():
     root.configure(bg='#88effc')
     root.mainloop()
 
-
-'''
-Class Login
-'''
-class Login(object):
-    def __init__(self, master):
-        self.master = master
-
-        self.Frame_Login = Frame(self.master, height=200)
-        self.Frame_Login.pack(fill=X)
-        self.Label_Login = Label(self.Frame_Login, text="Welcome to Store Mini", fg='red', font="arial 20 bold")
-        self.Label_Login.place(relx=0.1, rely=0.02)
-        self.Label_User = Label(self.Frame_Login, text="Username: ", font=('time new roman', 17))
-        self.Label_User.place(relx=0.01, rely=0.3)
-        self.Label_Pass = Label(self.Frame_Login, text="Password: ", font=('time new roman', 17))
-        self.Label_Pass.place(relx=0.01, rely=0.55)
-
-        self.user = StringVar()
-        self.passw = StringVar()
-
-        self.Entry_User = Entry(self.Frame_Login, bg="white", bd=1,font=('Arial', 24), textvariable=self.user)
-        self.Entry_User.place(relx=0.3, rely=0.3, relwidth=0.6, relheight=0.15)
-        self.Entry_Pass = Entry(self.Frame_Login, bg="white", bd=1,font=('Arial', 24), show="*", textvariable=self.passw)
-        self.Entry_Pass.place(relx=0.3, rely=0.55, relwidth=0.6, relheight=0.15)
-
-        self.Btn_Login = Button(self.Frame_Login, text="Sign In",font=('Arial', 18), fg="Blue", command=self.Onclick)
-        self.Btn_Login.place(relx=0.25, rely=0.8, relwidth=0.3, relheight=0.15)
-        self.Btn_Cancel = Button(self.Frame_Login, text="Cancel",font=('Arial', 18), fg="Red", command=self.master.destroy)
-        self.Btn_Cancel.place(relx=0.6, rely=0.8, relwidth=0.3, relheight=0.15)
-
-
-    '''
-    User and Password
-    '''
-    def Onclick(self):
-        UserName = self.Entry_User.get()
-        Passw = self.Entry_Pass.get()
-        if (UserName == "admin" and Passw == "admin"):
-            return self.Open_FrameMenu()
-        elif (UserName == "" or Passw == ""):
-            return messagebox.showerror("Error", "You have not entered full information")
-        else:
-            return messagebox.showerror("Error", "You entered incorrect account information")
-
-    def Open_FrameMenu(self):
-        self.master.destroy()
-        mainwindow()
-
-
-
-def main_Login():
-    Root_Login=Tk()
-    Login(Root_Login)
-    Root_Login.iconbitmap('shop.ico')
-    Root_Login.title("Mini Shop")
-    Root_Login.geometry("400x200")
-    Root_Login.resizable(False, False)
-    Root_Login.mainloop()
-
-
 if __name__ == "__main__":
-    main_Login()
+    mainwindow()
+
+
+# '''
+# Class Login
+# '''
+# class Login(object):
+#     def __init__(self, master):
+#         self.master = master
+#
+#         self.Frame_Login = Frame(self.master, height=200)
+#         self.Frame_Login.pack(fill=X)
+#         self.Label_Login = Label(self.Frame_Login, text="Welcome to Store Mini", fg='red', font="arial 20 bold")
+#         self.Label_Login.place(relx=0.1, rely=0.02)
+#         self.Label_User = Label(self.Frame_Login, text="Username: ", font=('time new roman', 17))
+#         self.Label_User.place(relx=0.01, rely=0.3)
+#         self.Label_Pass = Label(self.Frame_Login, text="Password: ", font=('time new roman', 17))
+#         self.Label_Pass.place(relx=0.01, rely=0.55)
+#
+#         self.user = StringVar()
+#         self.passw = StringVar()
+#
+#         self.Entry_User = Entry(self.Frame_Login, bg="white", bd=1,font=('Arial', 24), textvariable=self.user)
+#         self.Entry_User.place(relx=0.3, rely=0.3, relwidth=0.6, relheight=0.15)
+#         self.Entry_Pass = Entry(self.Frame_Login, bg="white", bd=1,font=('Arial', 24), show="*", textvariable=self.passw)
+#         self.Entry_Pass.place(relx=0.3, rely=0.55, relwidth=0.6, relheight=0.15)
+#
+#         self.Btn_Login = Button(self.Frame_Login, text="Sign In",font=('Arial', 18), fg="Blue", command=self.Onclick)
+#         self.Btn_Login.place(relx=0.25, rely=0.8, relwidth=0.3, relheight=0.15)
+#         self.Btn_Cancel = Button(self.Frame_Login, text="Cancel",font=('Arial', 18), fg="Red", command=self.master.destroy)
+#         self.Btn_Cancel.place(relx=0.6, rely=0.8, relwidth=0.3, relheight=0.15)
+#
+#
+#     '''
+#     User and Password
+#     '''
+#     def Onclick(self, event=None):
+#         UserName = self.Entry_User.get()
+#         Passw = self.Entry_Pass.get()
+#         if (UserName == "admin" and Passw == "admin"):
+#             return self.Open_FrameMenu()
+#         elif (UserName == "" or Passw == ""):
+#             return messagebox.showerror("Error", "You have not entered full information")
+#         else:
+#             return messagebox.showerror("Error", "You entered incorrect account information")
+#
+#     def Open_FrameMenu(self):
+#
+#         self.master.destroy()
+#         mainwindow()
+#
+#
+#
+# def main_Login():
+#     Root_Login=Tk()
+#     Login(Root_Login)
+#     Root_Login.iconbitmap('shop.ico')
+#     Root_Login.title("Mini Shop")
+#     Root_Login.geometry("400x200")
+#     Root_Login.resizable(False, False)
+#     Root_Login.mainloop()
+#
+#
+# if __name__ == "__main__":
+#     main_Login()
 
 
 
